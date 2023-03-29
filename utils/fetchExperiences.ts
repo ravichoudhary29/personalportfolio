a053a33329c0507a86b4baf127ebf6c93594cc7f
Ravi Kumar <kumar.ravi.vegan@gmail.com>
2023-03-29 15:32:01 +0530

    Remove NEXT_PUBLIC_BASE_URL env variables from nextjs api routes

--- utils/fetchExperiences.ts
+++ utils/fetchExperiences.ts
@@ -1,9 +1,7 @@
 import { Experience } from "../typings";
 
 export const fetchExperiences = async () => {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/getExperience`
-  );
+  const res = await fetch(`/api/getExperience`);
 
   const data = await res.json();
   const experiences: Experience[] = data.experiences;
--- utils/fetchPageInfo.ts
+++ utils/fetchPageInfo.ts
@@ -1,20 +1,12 @@
 import { PageInfo } from "../typings";
 
 export const fetchPageInfo = async () => {
-  try {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/getPageInfo`
-    );
+  const res = await fetch(`/api/getPageInfo`);
 
-    const data = await res.json();
-    const pageInfo: PageInfo = data.pageInfo;
+  const data = await res.json();
+  const pageInfo: PageInfo = data.pageInfo;
 
-    // console.log("fetching", pageInfo);
+  //   console.log("fetching", pageInfo);
 
-    return pageInfo;
-  } catch (error) {
-    console.error(error);
-    // Handle the error in some way, such as showing an error message to the user
-    return null;
-  }
+  return pageInfo;
 };
